d1cdd5fe85aff61c80671613b75ef162e038b36d
jde-maga <d.julien78+github@gmail.com>
2018-10-09 17:59:23 +0200

    heroku stop idle

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const https = require("https");
 const express = require("express");
 const app = express();
 
@@ -9,4 +10,8 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log("WoW Raid Helper server listening on 3000");
+  setInterval(() => {
+    https.get("https://wow-raid-helper.herokuapp.com/");
+    console.log("Stop Idling Heroku");
+  }, 300000);
 });
